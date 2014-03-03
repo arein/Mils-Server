2dd0a0b3eaf2e61dfd1b2e1935e6094f4f6dec34
Alexander-Derek Rein <adr@ceseros.de>
2014-03-03 23:19:37 +0100

    Misc Changes

--- docsaway-nodejs/lib/manualPricing.php
+++ docsaway-nodejs/lib/manualPricing.php
@@ -4,7 +4,11 @@ $arr = array();
 try
 {
     $json = json_decode($argv[1]);
-    $client = new SoapClient('https://www.docsaway.com/app/api/soap/api_pricing.wsdl', array('trace' => true, "connection_timeout" => 180));
+    $context = array(
+        'ssl' => array('verify_peer' => false,
+            'allow_self_signed' => true)
+    );
+    $client = new SoapClient('https://www.docsaway.com/app/api/soap/api_pricing.wsdl', array('trace' => true, "connection_timeout" => 180, 'stream_context' => stream_context_create($context)));
     $client->setAPIConnection($json->credentials->email, $json->credentials->installationKey);
     $client->setStation($json->station->station);
     $client->setCourier($json->station->courier);
--- docsaway-nodejs/lib/stationFinder.php
+++ docsaway-nodejs/lib/stationFinder.php
@@ -5,7 +5,11 @@ $arr = array();
 try
 {
     $json = json_decode($argv[1]);
-    $client = new SoapClient('https://www.docsaway.com/app/api/soap/api_station_finder.wsdl', array('trace' => true, "connection_timeout" => 180));
+    $context = array(
+        'ssl' => array('verify_peer' => false,
+            'allow_self_signed' => true)
+    );
+    $client = new SoapClient('https://www.docsaway.com/app/api/soap/api_station_finder.wsdl', array('trace' => true, "connection_timeout" => 180, 'stream_context' => stream_context_create($context)));
     $client->setAPIConnection($json->credentials->email, $json->credentials->installationKey);
     $client->setColumnSeparator('$$$$$');
     $client->setRowSeparator('\n');
