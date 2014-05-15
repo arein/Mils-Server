9e5d79de5d335f068b6be5c3d6d82c7ad49775a5
Alexander-Derek Rein <adr@ceseros.de>
2014-05-15 21:37:07 +0200

    Resetted to master

--- src/util/mail/policy.js
+++ src/util/mail/policy.js
@@ -1,8 +1,5 @@
 var Docsaway = require('./strategies/docsaway');
-var SmsKaufen = require('./strategies/sms_kaufen');
-var Lob = require('./strategies/lob');
-var Ezgram = require('./strategies/ezgram');
-var Postful = require('./strategies/postful');
+
 var Policy = (function () {
     function Policy(theContext) {
         this.theContext = theContext;
@@ -10,26 +7,6 @@ var Policy = (function () {
     }
     Policy.prototype.configure = function (destinationCountry) {
         switch (destinationCountry.toLowerCase()) {
-            case "de":
-            case "at":
-            case "ch":
-                this.context.mailStrategy = new SmsKaufen();
-                break;
-            case "us":
-            case "ca":
-            case "mx":
-                this.context.mailStrategy = new Lob();
-                break;
-            case "nl":
-            case "be":
-            case "fr":
-                this.context.mailStrategy = new Ezgram();
-                break;
-            case "cn":
-            case "sg":
-            case "jp":
-                this.context.mailStrategy = new Postful();
-                break;
             default:
                 this.context.mailStrategy = new Docsaway();
                 break;
--- src/util/mail/policy.ts
+++ src/util/mail/policy.ts
@@ -14,26 +14,6 @@ class Policy {
 
     configure(destinationCountry:string) {
         switch  (destinationCountry.toLowerCase()) {
-            case "de":
-            case "at":
-            case "ch":
-                this.context.mailStrategy = new SmsKaufen();
-                break;
-            case "us":
-            case "ca":
-            case "mx":
-                this.context.mailStrategy = new Lob();
-                break;
-            case "nl":
-            case "be":
-            case "fr":
-                this.context.mailStrategy = new Ezgram();
-                break;
-            case "cn":
-            case "sg":
-            case "jp":
-                this.context.mailStrategy = new Postful();
-                break;
             default:
                 this.context.mailStrategy = new Docsaway();
                 break;
