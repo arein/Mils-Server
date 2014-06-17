a521c7fb6392e4dab5e48313809e6897ad391493
Alexander-Derek Rein <adr@ceseros.de>
2014-06-17 11:09:13 +0200

    Push Notification Improvements

--- src/manager/NotificationManager.js
+++ src/manager/NotificationManager.js
@@ -1,3 +1,5 @@
+var MongoManager = require("./MongoManager");
+
 var MailManager = require("./MailManager");
 var Config = require("./../config");
 var ClientType = require("./../model/ClientType");
@@ -15,15 +17,29 @@ var NotificationManager = (function () {
                         // TODO: Save the letter
                         letter.printInformation.dispatchedByPrintingProvider = true;
                         letter.printInformation.dispatchedByPrintingProviderAt = dispatchDate;
-                        NotificationManager.notifyCustomerViaEmail(letter);
-                        NotificationManager.notifyCustomerViaPushNotification(letter);
+
+                        NotificationManager.notifyCustomerViaEmail(letter, function (error) {
+                            if (!error) {
+                                NotificationManager.notifyCustomerViaPushNotification(letter);
+
+                                MongoManager.getDb(function (db) {
+                                    db.collection('letter', function (err, collection) {
+                                        collection.update({ '_id': letter._id }, letter, { safe: true }, function (err, result) {
+                                            if (err) {
+                                                console.log(err);
+                                            }
+                                        });
+                                    });
+                                });
+                            }
+                        });
                     }
                 });
             }
         });
     };
 
-    NotificationManager.notifyCustomerViaEmail = function (letter) {
+    NotificationManager.notifyCustomerViaEmail = function (letter, callback) {
         var nodemailer = require("nodemailer");
         var jade = require('jade');
         var dateFormat = require('dateformat');
@@ -60,51 +76,52 @@ var NotificationManager = (function () {
             smtpTransport.sendMail(mailOptions, function (error, response) {
                 // if you don't want to use this transport object anymore, uncomment following line
                 smtpTransport.close(); // shut down the connection pool, no more messages
-                if (error)
-                    throw err;
+                callback(error);
             });
         });
     };
 
     NotificationManager.notifyCustomerViaPushNotification = function (letter) {
-        for (var i = 0; i < letter.devices.length; i++) {
-            var device = letter.devices[i];
-            if (device.type == 0 /* Windows81 */) {
-                var wns = require('wns');
+        if (typeof letter.devices !== "undefined") {
+            for (var i = 0; i < letter.devices.length; i++) {
+                var device = letter.devices[i];
+                if (device.type == 0 /* Windows81 */) {
+                    var wns = require('wns');
 
-                var channelUrl = device.uri;
-                var options = {
-                    client_id: 'ms-app://s-1-15-2-1797842556-2978483067-2652608984-700972092-662318483-3541751713-3387607526',
-                    client_secret: 'OG53FJdqtCkjKt0dtNZyuMrUt2wWhNE6'
-                };
+                    var channelUrl = device.uri;
+                    var options = {
+                        client_id: 'ms-app://s-1-15-2-1797842556-2978483067-2652608984-700972092-662318483-3541751713-3387607526',
+                        client_secret: 'OG53FJdqtCkjKt0dtNZyuMrUt2wWhNE6'
+                    };
 
-                wns.sendToastText01(channelUrl, {
-                    text1: 'Your letter was successfully dispatched'
-                }, options, function (error, result) {
-                    if (error)
-                        console.error(error);
-                    else
-                        console.log(result);
-                });
-            } else if (device.type == 1 /* MacOS1010 */) {
-                var apn = require('apn');
-                var prefix = Config.isProd() ? "production" : "sandbox";
-                var opt = {
-                    production: Config.isProd(),
-                    cert: Config.getBasePath() + "/private/encryption/" + prefix + ".cert.pem",
-                    key: Config.getBasePath() + "/private/encryption/" + prefix + ".key.pem"
-                };
+                    wns.sendToastText01(channelUrl, {
+                        text1: 'Your letter was successfully dispatched'
+                    }, options, function (error, result) {
+                        if (error)
+                            console.error(error);
+                        else
+                            console.log(result);
+                    });
+                } else if (device.type == 1 /* MacOS1010 */) {
+                    var apn = require('apn');
+                    var prefix = Config.isProd() ? "production" : "sandbox";
+                    var opt = {
+                        production: Config.isProd(),
+                        cert: Config.getBasePath() + "/private/encryption/" + prefix + ".cert.pem",
+                        key: Config.getBasePath() + "/private/encryption/" + prefix + ".key.pem"
+                    };
 
-                var apnConnection = new apn.Connection(opt);
-                var myDevice = new apn.Device(device.uri);
-                var note = new apn.Notification();
+                    var apnConnection = new apn.Connection(opt);
+                    var myDevice = new apn.Device(device.uri);
+                    var note = new apn.Notification();
 
-                note.expiry = Math.floor(Date.now() / 1000) + 3600 * 24; // Expires 24 hour from now.
-                note.badge = 1;
-                note.alert = "Your letter was dispatched";
-                note.payload = { 'remoteLetterId': letter._id };
+                    note.expiry = Math.floor(Date.now() / 1000) + 3600 * 24; // Expires 24 hour from now.
+                    note.badge = 1;
+                    note.alert = "Your letter was dispatched";
+                    note.payload = { 'remoteLetterId': letter._id };
 
-                apnConnection.pushNotification(note, myDevice);
+                    apnConnection.pushNotification(note, myDevice);
+                }
             }
         }
     };
--- src/manager/NotificationManager.ts
+++ src/manager/NotificationManager.ts
@@ -17,15 +17,30 @@ class NotificationManager {
                         // TODO: Save the letter
                         letter.printInformation.dispatchedByPrintingProvider = true;
                         letter.printInformation.dispatchedByPrintingProviderAt = dispatchDate;
-                        NotificationManager.notifyCustomerViaEmail(letter);
-                        NotificationManager.notifyCustomerViaPushNotification(letter);
+
+                        NotificationManager.notifyCustomerViaEmail(letter, function(error: Error) {
+                            if (!error) {
+                                NotificationManager.notifyCustomerViaPushNotification(letter);
+
+                                MongoManager.getDb(function (db:mongo.Db) {
+                                    db.collection('letter', function (err:Error, collection) {
+                                        collection.update({'_id': letter._id}, letter, {safe: true}, function (err:Error, result:number) {
+                                            if (err) {
+                                                console.log(err);
+                                            }
+                                        });
+                                    });
+                                });
+                            }
+                        });
+
                     }
                 });
             }
         });
     }
 
-    public static notifyCustomerViaEmail(letter: Letter) {
+    public static notifyCustomerViaEmail(letter: Letter, callback: (error: Error) => void) {
         var nodemailer = require("nodemailer");
         var jade = require('jade');
         var dateFormat = require('dateformat');
@@ -58,50 +73,52 @@ class NotificationManager {
             smtpTransport.sendMail(mailOptions, function(error, response){
                 // if you don't want to use this transport object anymore, uncomment following line
                 smtpTransport.close(); // shut down the connection pool, no more messages
-                if(error) throw err;
+                callback(error);
             });
         });
     }
 
     public static notifyCustomerViaPushNotification(letter: Letter) {
-        for (var i = 0; i < letter.devices.length; i++) {
-            var device = letter.devices[i];
-            if (device.type == ClientType.ClientType.Windows81) {
-                var wns = require('wns');
+        if (typeof letter.devices !== "undefined") {
+            for (var i = 0; i < letter.devices.length; i++) {
+                var device = letter.devices[i];
+                if (device.type == ClientType.ClientType.Windows81) {
+                    var wns = require('wns');
 
-                var channelUrl = device.uri;
-                var options = {
-                    client_id: 'ms-app://s-1-15-2-1797842556-2978483067-2652608984-700972092-662318483-3541751713-3387607526',
-                    client_secret: 'OG53FJdqtCkjKt0dtNZyuMrUt2wWhNE6'
-                };
+                    var channelUrl = device.uri;
+                    var options = {
+                        client_id: 'ms-app://s-1-15-2-1797842556-2978483067-2652608984-700972092-662318483-3541751713-3387607526',
+                        client_secret: 'OG53FJdqtCkjKt0dtNZyuMrUt2wWhNE6'
+                    };
 
-                wns.sendToastText01(channelUrl, {
-                    text1: 'Your letter was successfully dispatched'
-                }, options, function (error, result) {
-                    if (error)
-                        console.error(error);
-                    else
-                        console.log(result);
-                });
-            } else if (device.type == ClientType.ClientType.MacOS1010) {
-                var apn = require('apn');
-                var prefix = Config.isProd() ? "production" : "sandbox";
-                var opt = {
-                    production: Config.isProd(),
-                    cert: Config.getBasePath() + "/private/encryption/" + prefix + ".cert.pem",
-                    key: Config.getBasePath() + "/private/encryption/" + prefix + ".key.pem"
-                };
+                    wns.sendToastText01(channelUrl, {
+                        text1: 'Your letter was successfully dispatched'
+                    }, options, function (error, result) {
+                        if (error)
+                            console.error(error);
+                        else
+                            console.log(result);
+                    });
+                } else if (device.type == ClientType.ClientType.MacOS1010) {
+                    var apn = require('apn');
+                    var prefix = Config.isProd() ? "production" : "sandbox";
+                    var opt = {
+                        production: Config.isProd(),
+                        cert: Config.getBasePath() + "/private/encryption/" + prefix + ".cert.pem",
+                        key: Config.getBasePath() + "/private/encryption/" + prefix + ".key.pem"
+                    };
 
-                var apnConnection = new apn.Connection(opt);
-                var myDevice = new apn.Device(device.uri);
-                var note = new apn.Notification();
+                    var apnConnection = new apn.Connection(opt);
+                    var myDevice = new apn.Device(device.uri);
+                    var note = new apn.Notification();
 
-                note.expiry = Math.floor(Date.now() / 1000) + 3600 * 24; // Expires 24 hour from now.
-                note.badge = 1;
-                note.alert = "Your letter was dispatched";
-                note.payload = {'remoteLetterId': letter._id};
+                    note.expiry = Math.floor(Date.now() / 1000) + 3600 * 24; // Expires 24 hour from now.
+                    note.badge = 1;
+                    note.alert = "Your letter was dispatched";
+                    note.payload = {'remoteLetterId': letter._id};
 
-                apnConnection.pushNotification(note, myDevice);
+                    apnConnection.pushNotification(note, myDevice);
+                }
             }
         }
     }
