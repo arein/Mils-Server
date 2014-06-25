2857fbad248ebc77602af5270c270193538da5a6
Alexander-Derek Rein <adr@ceseros.de>
2014-06-25 17:42:13 +0200

    Bug Fixes

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ app.use(express.urlencoded({ limit: '70mb' }));
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view options', { pretty: config.isProd() });
+app.locals.pretty = !config.isProd();
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
