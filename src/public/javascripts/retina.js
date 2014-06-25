4e0fe586328350c2e531b287d674cab1206fd34c
Alexander-Derek Rein <adr@ceseros.de>
2014-06-26 00:16:19 +0200

    Retina Optimizations

---
+++ src/public/javascripts/retina.js
@@ -0,0 +1,22 @@
+/**
+ * Created by arein on 25/06/14.
+ */
+
+Retina = function() {
+    return {
+        init: function(){
+            //Get pixel ratio and perform retina replacement
+            //Optionally, you may also check a cookie to see if the user has opted out of (or in to) retina support
+            var pixelRatio = !!window.devicePixelRatio ? window.devicePixelRatio : 1;
+            if (pixelRatio > 1) {
+                $("img").each(function(idx, el){
+                    el = $(el);
+                    if (el.attr("data-src2x")) {
+                        el.attr("data-src-orig", el.attr("src"));
+                        el.attr("src", el.attr("data-src2x"));
+                    }
+                });
+            }
+        }
+    };
+}();
