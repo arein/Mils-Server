16f33ea120bce020f5ff4375acdb8cf614db878d
Alexander-Derek Rein <adr@ceseros.de>
2014-08-28 17:59:01 -0700

    Added Videos

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -30,6 +30,7 @@ $(document).ready(function() {
 	$("#pages").change(pagesChanged);
 	$("#countries").change(pagesChanged);
 	queryPrice();
+    loadGif();
 });
 
 function pagesChanged() {
@@ -120,3 +121,52 @@ function hideLoading() {
     };
     document.SpriteSpinner = SpriteSpinner;
 })();
+
+function loadGif() {
+    /*
+    var counter = 0;
+    var loaded = function () {
+        counter++;
+        if (counter == 4) {
+            var osxframe = (isRetina()) ? "/images/mac-frame-2x.png" : "/images/mac-frame.png";
+            var osx = (isRetina()) ? "/images/Mac-2x.mp4" : "/images/Mac.mp4";
+            $('#osxframe').after('<img src="' + osxframe + '" width="348" style="margin-bottom: -117px" /><video width="320" height="200" controls="controls" autoplay="autoplay" style="position: relative;margin-top: -185px;margin-left: 2px;"><source src="' + osx +'" type="video/mp4"></video>');
+            $('#osxframe').hide();
+
+
+            var win8frame = (isRetina()) ? "/images/win-frame-2x.png" : "/images/win-frame.png";
+            var win8 = (isRetina()) ? "/images/Win8-2x.mp4" : "/images/Win8.mp4";
+            $('#winframe').after('<img src="' + win8frame + '" width="348" style="margin-bottom: -87px" /><video width="320" height="200" controls="controls" autoplay="autoplay" style="position: relative;margin-top: -185px;margin-left: 2px;"><source src="' + win8 +'" type="video/mp4"></video>');
+            $('#winframe').hide();
+
+            $('#macbutton').css('margin-top', '-5px');
+            $('#winbutton').css('margin-top', '-3px');
+        }
+    };
+
+    var mac = new Image();
+    mac.onload = loaded;
+    mac.src = (isRetina()) ? "/images/Mac-2x.gif" : "/images/Mac.gif";
+    var macFrame = new Image();
+    macFrame.onload = loaded;
+    macFrame.src = (isRetina()) ? "/images/mac-frame-2x.png" : "/images/mac-frame.png";;
+
+    var win = new Image();
+    win.onload = loaded;
+    win.src = (isRetina()) ? "/images/Win8-2x.gif" : "/images/Win8.gif";
+    var winFrame = new Image();
+    winFrame.onload = loaded;
+    winFrame.src = (isRetina()) ? "/images/win-frame-2x.png" : "/images/win-frame.png";
+    */
+}
+
+function isRetina() {
+    if (window.matchMedia) {
+        var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+        if (mq && mq.matches || (window.devicePixelRatio > 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
