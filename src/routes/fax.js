1c15bb809b94ac5781d18346da2e211af473f0a3
Alexander-Derek Rein <adr@ceseros.de>
2015-01-10 13:57:47 +0100

    Fax Routes added

---
+++ src/routes/fax.js
@@ -0,0 +1,35 @@
+/// <reference path='./../../vendor/typescript-node-definitions/node.d.ts'/>
+/// <reference path='./../../vendor/typescript-node-definitions/mongodb.d.ts'/>
+/// <reference path='./../../vendor/typescript-node-definitions/express3.d.ts'/>
+/**
+* Endpoint to purchase a letter.
+* @param req
+* @param res
+*/
+exports.getPrice = function (req, res) {
+};
+
+/**
+* Endpoint to upload a letter.
+* @param req
+* @param res
+*/
+exports.uploadFax = function (req, res) {
+};
+
+/**
+* Endpoint to upload a letter.
+* @param req
+* @param res
+*/
+exports.getIapSku = function (req, res) {
+};
+
+/**
+* Endpoint to upload a letter.
+* @param req
+* @param res
+*/
+exports.confirmPurchase = function (req, res) {
+};
+//# sourceMappingURL=fax.js.map
---
+++ src/routes/fax.ts
@@ -0,0 +1,43 @@
+/// <reference path='./../../vendor/typescript-node-definitions/node.d.ts'/>
+/// <reference path='./../../vendor/typescript-node-definitions/mongodb.d.ts'/>
+/// <reference path='./../../vendor/typescript-node-definitions/express3.d.ts'/>
+
+import express = require("express3")
+import mongo = require("mongodb")
+import ObjectId = mongo.ObjectID
+
+/**
+ * Endpoint to purchase a letter.
+ * @param req
+ * @param res
+ */
+exports.getPrice = function(req : express.Request, res : express.Response) {
+
+};
+
+/**
+ * Endpoint to upload a letter.
+ * @param req
+ * @param res
+ */
+exports.uploadFax = function(req : express.Request, res : express.Response) {
+
+};
+
+/**
+ * Endpoint to upload a letter.
+ * @param req
+ * @param res
+ */
+exports.getIapSku = function(req : express.Request, res : express.Response) {
+
+};
+
+/**
+ * Endpoint to upload a letter.
+ * @param req
+ * @param res
+ */
+exports.confirmPurchase = function(req : express.Request, res : express.Response) {
+
+};
